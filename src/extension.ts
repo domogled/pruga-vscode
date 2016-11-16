11dc5a6e07bf8a2cb0af739c229c13167e04e2a6
Петр Болф <petr.bolf@domogled.com>
2016-11-16 04:18:06 +0100

    automaticky start livereload

--- src/extension.ts
+++ src/extension.ts
@@ -13,6 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "pruga-vscode" is now active!');
 
+    const config = vscode.workspace.getConfiguration('pruga')
+    const autoStart = config.get('livereload.autoStart')
+    if(autoStart) {
+        vscode.commands.executeCommand('pruga.livereload')
+    }
+
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
